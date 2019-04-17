e5a753f1a2c64c5e5f8cf1111eee75f09803d5b4
daniel <danil_timchenko_1997@mail.ru>
2019-04-17 21:41:02 +0700

    Create input main_row function in turing.cpp

--- src/turing.cpp
+++ src/turing.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N = 10000;
+string main_row;
 struct action
 {
   string state;
@@ -13,27 +14,27 @@ int check_ERROR(action* rules, int j)
 {
   if (rules[j].state.length() > 2)
   {
-    cout << "Error in line = " << j+1 << " in state";
+    cout << "Error in line = " << j+1 << " in state" << endl;
     return 0;
   }
   if (rules[j].expected_symbol.length() > 1)
   {
-    cout << "Error in line = " << j+1 << " in expected_symbol";
+    cout << "Error in line = " << j+1 << " in expected_symbol" << endl;
     return 0;
   }
   if (rules[j].new_symbol.length() > 1)
   {
-    cout << "Error in line = " << j+1 << " in new_symbol";
+    cout << "Error in line = " << j+1 << " in new_symbol" << endl;
     return 0;
   }
   if (rules[j].move.length() > 1 || (rules[j].move != "r" && rules[j].move != "l" && rules[j].move != "n"))
   {
-    cout << "Error in line = " << j+1 << " in move";
+    cout << "Error in line = " << j+1 << " in move" << endl;
     return 0;
   }
   if (rules[j].next_state.length() > 2)
   {
-    cout << "Error in line = " << j+1 << " next_state";
+    cout << "Error in line = " << j+1 << " next_state" << endl;
     return 0;
   }
   return 1;
@@ -56,11 +57,17 @@ void input_rules(action* rules)
       break;
   }
 }
+void input_main_row()
+{
+  cout << "Введите строку для обработки!" << endl;
+  cin >> main_row;
+}
 int main()
 {
   setlocale(LC_ALL,"russian");
   action *rules;
   rules = new action [N];
+  input_main_row();
   input_rules(rules);
   delete []rules;
   return 0;
