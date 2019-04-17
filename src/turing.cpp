262e027f6d5f1fa28999439d8914d43e4d965e53
daniel <danil_timchenko_1997@mail.ru>
2019-04-17 21:12:46 +0700

    Input_rules function in turing.cpp

--- src/turing.cpp
+++ src/turing.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+int N = 10000;
 struct action
 {
   string state;
@@ -8,8 +9,24 @@ struct action
   string move;
   string next_state;
 };
+void input_rules(action* rules)
+{
+  for (int i = 0; i < N; i++)
+  {
+    cin >> rules[i].state;
+    cin >> rules[i].expected_symvol;
+    cin >> rules[i].new_symbol;
+    cin >> rules[i].move;
+    cin >> rules[i].next_state;
+    check_ERROR(rules,i);
+  }
+}
 int main()
 {
-
+  setlocale(LC_ALL,"russian");
+  action *rules;
+  rules = new action [N];
+  input_rules(rules);
+  delete []rules;
   return 0;
 }
