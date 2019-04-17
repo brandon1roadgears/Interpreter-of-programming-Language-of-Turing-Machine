d5d21bc557e116dc94834494b82d0e7f6414dfab
daniel <danil_timchenko_1997@mail.ru>
2019-04-17 21:31:05 +0700

    Create check_Error function in turing.cpp

--- src/turing.cpp
+++ src/turing.cpp
@@ -4,21 +4,56 @@ int N = 10000;
 struct action
 {
   string state;
-  string expected_symvol;
+  string expected_symbol;
   string new_symbol;
   string move;
   string next_state;
 };
+int check_ERROR(action* rules, int j)
+{
+  if (rules[j].state.length() > 2)
+  {
+    cout << "Error in line = " << j+1 << " in state";
+    return 0;
+  }
+  if (rules[j].expected_symbol.length() > 1)
+  {
+    cout << "Error in line = " << j+1 << " in expected_symbol";
+    return 0;
+  }
+  if (rules[j].new_symbol.length() > 1)
+  {
+    cout << "Error in line = " << j+1 << " in new_symbol";
+    return 0;
+  }
+  if (rules[j].move.length() > 1 || (rules[j].move != "r" && rules[j].move != "l" && rules[j].move != "n"))
+  {
+    cout << "Error in line = " << j+1 << " in move";
+    return 0;
+  }
+  if (rules[j].next_state.length() > 2)
+  {
+    cout << "Error in line = " << j+1 << " next_state";
+    return 0;
+  }
+  return 1;
+}
 void input_rules(action* rules)
 {
+  cout << "Введите правила!" << endl;
+  int fl;
   for (int i = 0; i < N; i++)
   {
     cin >> rules[i].state;
-    cin >> rules[i].expected_symvol;
+    cin >> rules[i].expected_symbol;
     cin >> rules[i].new_symbol;
     cin >> rules[i].move;
     cin >> rules[i].next_state;
-    check_ERROR(rules,i);
+    fl = check_ERROR(rules,i);
+    if (!fl)
+      break;
+    if (rules[i].next_state == "##")
+      break;
   }
 }
 int main()
