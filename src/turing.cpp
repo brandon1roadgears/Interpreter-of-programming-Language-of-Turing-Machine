0ce639a5e8304e7a85538352776ec4bcaf0a984a
daniel <danil_timchenko_1997@mail.ru>
2019-04-17 20:49:26 +0700

    Create turing.cpp with struct and main

---
+++ src/turing.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct action
+{
+  string state;
+  string expected_symvol;
+  string new_symbol;
+  string move;
+  string next_state;
+};
+int main()
+{
+
+  return 0;
+}
