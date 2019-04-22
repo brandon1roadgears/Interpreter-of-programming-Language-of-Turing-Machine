09510dac47f7a9fbef3110d77a46493f3477d8f5
daniel <danil_timchenko_1997@mail.ru>
2019-04-22 20:09:29 +0700

    Add global struct with rules in head

--- src/head.h
+++ src/head.h
@@ -3,3 +3,10 @@ using namespace std;
 int N = 10000;
 int point = 0;
 string main_row;
+struct action {
+    string state;
+    string expected_symbol;
+    string new_symbol;
+    string move;
+    string next_state;
+};
