a8e244bbd88086a707bcc0a8ffaa80eaf0884a0e
daniel <danil_timchenko_1997@mail.ru>
2019-04-23 19:59:39 +0700

    Add externs in head.h

--- src/head.h
+++ src/head.h
@@ -10,3 +10,9 @@ struct action {
     string move;
     string next_state;
 };
+extern int check_ERROR(action* rules, int j);
+extern void printstring();
+extern void MOVE(action* rules, int j);
+extern void RUN(action* rules, int end);
+extern void input_main_row();
+extern void input_rules(action* rules);
