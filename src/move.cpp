3f069ebaee4f9cb2f55d4cb6a047f51073b094bd
daniel <danil_timchenko_1997@mail.ru>
2019-04-28 16:17:00 +0700

    Add body in move.cpp

---
+++ src/move.cpp
@@ -0,0 +1,11 @@
+#include "head.h"
+int move (int point, vector<action> &rules, int i)
+{
+	if (rules[i].move == "r")
+			point++;
+	if (rules[i].move =="l")
+			point--;
+	if (rules[i].move == "n")
+			point = point;
+	return point;
+}
