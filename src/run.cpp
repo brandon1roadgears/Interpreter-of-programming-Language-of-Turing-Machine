aa3481e5624c0bcaa68778fcf07df75c1b00b7a5
daniel <danil_timchenko_1997@mail.ru>
2019-05-11 15:35:55 +0700

    Add extern of do_s(f) and rewrite(f)

--- src/run.cpp
+++ src/run.cpp
@@ -1,22 +1,16 @@
 #include "head.h"
 extern void print_string(string row);
 extern int move(int point, vector<action>& rules, int i);
+extern string do_s(vector <action> &rules, string main_row, int i, int point);
 void RUN(vector<action>& rules, int end, string main_row)
 {
-    int point = 0;
-    string word1;
-    string word2;
-    for (int i = 0; i < end; i++) {
-        word1 = rules[i].exp_symbol;
-        word2 = rules[i].new_symb;
-        if (word1[0] != main_row[point]) {
-            cout << word1[0];
-            cout << "NO symbols in rules";
-            exit(1);
-        } else {
-            main_row[point] = word2[0];
-        }
-        point = move(point, rules, i);
-        print_string(main_row);
+  {
+    string current_state = rules[0].next_state;
+    for (int i = 0; i <= end; i++)
+    {
+          main_row = do_s(rules, main_row, i, point);
+          print_string(main_row);
+          point = move(  point,rules, i);
     }
+  }
 }
