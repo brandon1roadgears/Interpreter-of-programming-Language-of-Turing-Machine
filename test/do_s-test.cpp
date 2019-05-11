1646a5a075d2e17ed30405ca4a207734312c4f72
batya <dimaka.anikeev@gmail.com>
2019-05-11 23:39:41 +0700

    Create do_s-test.cpp with function

---
+++ test/do_s-test.cpp
@@ -0,0 +1,10 @@
+#include "turing-test-head.h"
+string do_s(string main_row, int point, string exp_symb, string new_symb)
+{
+	if (main_row[point] == exp_symb[0])
+	{
+		main_row[point] = new_symb[0];
+	}
+	return 
+}
+
