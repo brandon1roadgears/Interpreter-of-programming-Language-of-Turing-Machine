ea87c536b6a1fedda4153332c947661e202277e1
daniel <danil_timchenko_1997@mail.ru>
2019-05-11 15:41:15 +0700

    Add point in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@ extern string input_main_row(string main_row);
 extern void input_rules(vector<action>& rules, string main_row);
 int main()
 {
-    string main_row;
-    vector<action> rules(10000);
-    main_row = input_main_row(main_row);
-    input_rules(rules, main_row);
-
-    return 0;
+	int point = 0;
+	string main_row;
+	vector <action> rules(10000);
+	main_row = input_main_row(main_row);
+	input_rules(rules, main_row, point);
+	return 0;
 }
