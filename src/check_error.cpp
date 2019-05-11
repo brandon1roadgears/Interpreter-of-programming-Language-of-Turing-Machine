84ac986fe6a89e26cf2ea1f8c16384530ee7d0c7
daniel <danil_timchenko_1997@mail.ru>
2019-05-11 15:52:55 +0700

    Delete mistake in check_error && delete check_rules

--- src/check_error.cpp
+++ src/check_error.cpp
@@ -3,8 +3,9 @@ int check_error(vector<action>& rules, int i)
 {
     if (rules[0].state != "00") {
         cout << "Name of first state must be 00!!!" << endl;
+        return 0;
     }
-    if (rules[i].state.length() > 2) {
+    if (rules[i].state.length() > 10) {
         cout << "Error in line = " << i + 1 << " in state!!!" << endl;
         return 0;
     }
@@ -20,7 +21,7 @@ int check_error(vector<action>& rules, int i)
         cout << "Error in line = " << i + 1 << " in move!!!" << endl;
         return 0;
     }
-    if (rules[i].next_state.length() > 2) {
+    if (rules[i].next_state.length() > 10) {
         cout << "Error in line = " << i + 1 << " in next state!!!" << endl;
         return 0;
     }
