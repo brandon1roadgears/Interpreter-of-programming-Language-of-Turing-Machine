89afe95f1397b1814b670e6299ba4207d690a75c
batya <dimaka.anikeev@gmail.com>
2019-05-16 00:02:09 +0700

    Add progress by state

--- src/run.cpp
+++ src/run.cpp
@@ -4,10 +4,19 @@ extern int move(int point, vector<action>& rules, int i);
 extern string do_s(vector<action>& rules, string main_row, int i, int point);
 void RUN(vector<action>& rules, int end, string main_row, int point)
 {
-    // string current_state = rules[0].next_state;
-    for (int i = 0; i <= end; i++) {
-        main_row = do_s(rules, main_row, i, point);
-        print_string(main_row);
-        point = move(point, rules, i);
+    int i = 0;
+    string current_state = rules[0].state;
+    while (current_state != "##")
+    {
+	if (current_state == rules[i].state && main_row[point] == rules[i].exp_symbol[0)
+	{
+        	main_row = do_s(rules, main_row, i, point);
+        	print_string(main_row);
+        	point = move(point, rules, i);
+		i = 0;
+	}else
+	    {
+		i++;
+	    }
     }
 }
