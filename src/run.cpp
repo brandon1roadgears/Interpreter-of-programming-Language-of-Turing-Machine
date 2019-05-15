02d1d90437e415af04577c9cd25be8333bdf97a4
batya <dimaka.anikeev@gmail.com>
2019-05-16 00:22:34 +0700

    Add point variable for run.cpp

--- src/run.cpp
+++ src/run.cpp
@@ -2,10 +2,10 @@
 extern void print_string(string row);
 extern int move(int point, vector<action>& rules, int i);
 extern string do_s(vector<action>& rules, string main_row, int i, int point);
-void RUN(vector<action>& rules, int end, string main_row, int point)
+void RUN(vector<action>& rules, int end, string main_row)
 {
-    int i = 0;
-    string current_state = rules[0].state;
+    int i = 0, point = 0;
+    string current_state = rules[point].state;
     while (current_state != "##")
     {
 	if (current_state == rules[i].state && main_row[point] == rules[i].exp_symbol[0)
