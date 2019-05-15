06b5b7b6676aed6d1ea7e5b1e8ab65098d0229e6
daniel <danil_timchenko_1997@mail.ru>
2019-05-16 02:13:13 +0700

    Change input_rules for dinamic array

--- src/input_rules.cpp
+++ src/input_rules.cpp
@@ -1,21 +1,23 @@
 #include "head.h"
 extern int check_error(vector<action>& rules, int i);
-extern void RUN(vector<action>& rules, int end, string main_row);
+extern void RUN(vector<action>& rules, string main_row);
 void input_rules(vector<action>& rules, string main_row)
 {
     int fl;
-    long unsigned int i;
-    for (i = 0; i < rules.size(); i++) {
+    for (i = 0; 1; i++)
+        rules.push_back(element);
         cin >> rules[i].state;
         cin >> rules[i].exp_symbol;
         cin >> rules[i].new_symb;
         cin >> rules[i].move;
         cin >> rules[i].next_state;
         fl = check_error(rules, i);
-        if (!fl)
+        if (!fl){
+            cout << "ERROR";
             break;
+        }
         if (rules[i].next_state == "##")
             break;
     }
-    RUN(rules, i, main_row);
+    RUN(rules, main_row);
 }
