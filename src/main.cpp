123d3bfbad52a89c15e4dafc40d1dd3c7c5217b1
daniel <danil_timchenko_1997@mail.ru>
2019-05-18 23:47:59 +0700

    Add new function in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include "head.h"
+extern void hello_user();
 extern string input_main_row(string main_row);
 extern void input_rules(vector<action>& rules, string main_row);
-extern void hello_user();
 int main()
 {
-    string main_row;
-    vector<action> rules;
-    hello_user();
-    main_row = input_main_row(main_row);
-    input_rules(rules, main_row);
-    return 0;
+	hello_user();
+	string main_row;
+	vector <action> rules;
+	main_row = input_main_row(main_row);
+	input_rules(rules, main_row);
+	return 0;
 }
