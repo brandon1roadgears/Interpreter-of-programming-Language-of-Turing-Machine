8261622cd6252883974466fdff9b9c89c9e15d5b
daniel <danil_timchenko_1997@mail.ru>
2019-05-18 23:45:52 +0700

    Add reading from file in input_main_row.cpp

--- src/input_main_row.cpp
+++ src/input_main_row.cpp
@@ -1,7 +1,8 @@
 #include "head.h"
 string input_main_row(string main_row)
 {
-    cout << "Input main Row" << endl;
-    cin >> main_row;
-    return main_row;
+	cout << "Input main Row" << endl;
+		ifstream str("string.txt");
+		getline(str,main_row);
+	return main_row;
 }
