a6b1ad293430f79b5828887114643f4b196417f5
Dimka <dimaka.anikeev@gmail.com>
2019-05-19 15:36:29 +0700

    Once more mistake in tests

--- test/check_error-test.cpp
+++ test/check_error-test.cpp
@@ -7,7 +7,7 @@ bool check_error(vector <action> &rules)
 		cout << "Name of first state must be 00 !!!" << endl;
 		return 0;
 	}
-	for (long unsigned int i = 0; i < rules.size() - 1; i++)
+	for (long unsigned int i = 0; i < rules.size(); i++)
 	{
 		if (rules[i].state.length() > 10)
 		{
