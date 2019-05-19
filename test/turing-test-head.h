18e8db66437371233e851001b7f1cae7071d1605
Dimka <dimaka.anikeev@gmail.com>
2019-05-19 14:38:58 +0700

    Add struct in ...test-head.h file

--- test/turing-test-head.h
+++ test/turing-test-head.h
@@ -1,4 +1,11 @@
 #pragma once
 #include "catch.hpp"
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+struct action{
+	std::string state;
+	std::string exp_symbol;
+	std::string new_symb;
+	std::string move;
+	std::string next_state;
+};
