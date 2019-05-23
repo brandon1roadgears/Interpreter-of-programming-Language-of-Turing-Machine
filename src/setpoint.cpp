7c4b390baea09e728608a008fcaddfd10864668e
daniel <danil_timchenko_1997@mail.ru>
2019-05-23 14:40:17 +0700

    Clang setpoint.cpp

--- src/setpoint.cpp
+++ src/setpoint.cpp
@@ -1,9 +1,9 @@
 #include "head.h"
 int setpoint(string main_row)
 {
-  int i = 0;
-  while(main_row[i] == '_')
-    ++i;
-  cout << "POINT = " << i << endl;
-  return i;
+    int i = 0;
+    while (main_row[i] == '_')
+        ++i;
+    cout << "POINT = " << i << endl;
+    return i;
 }
