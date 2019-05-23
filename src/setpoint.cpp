d25194704f82c98298ecf7df50011f89ad34fe99
Dimka <dimaka.anikeev@gmail.com>
2019-05-23 15:28:25 +0700

    New inf about version of the program

--- src/setpoint.cpp
+++ src/setpoint.cpp
@@ -4,6 +4,5 @@ int setpoint(string main_row)
     int i = 0;
     while (main_row[i] == '_')
         ++i;
-    cout << "POINT = " << i << endl;
     return i;
 }
--- test/setpoint-test.cpp
+++ test/setpoint-test.cpp
@@ -4,7 +4,6 @@ int setpoint(string main_row)
     int i = 0;
     while (main_row[i] == '_')
         ++i;
-    cout << "POINT = " << i << endl;
     return i;
 }
 
